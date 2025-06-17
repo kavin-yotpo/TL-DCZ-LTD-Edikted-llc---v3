3c8b73291c814540b25c692507465ce78e78724b
Kavin-crew <abreganakavin@gmail.com>
2025-06-17 23:22:13 +0800

    init files

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -44,7 +44,6 @@ function StarRating(props) {
           data-bs-parent="#accordionExample"
         >
           <div className="accordion-body">
-
             <div className="rating-holder">
               <section>
                 <h1>Product Star Rating</h1>
@@ -53,17 +52,6 @@ function StarRating(props) {
                   data-yotpo-instance-id={instanceid}
                   data-yotpo-product-id={productid}
                   data-yotpo-section-id="product"
-                  mode-preview={productid === "" ? "true" : ""}
-                ></div>
-              </section>
-
-              <section>
-                <h1>Collections Star Rating</h1>
-                <div
-                  className="yotpo-widget-instance"
-                  data-yotpo-instance-id={instanceid}
-                  data-yotpo-product-id={productid}
-                  data-yotpo-section-id="collection"
                   mode-preview={productid === "" ? "true" : ""}
                 ></div>
               </section>
